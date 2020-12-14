39fae94998872e99c23b334330d0fccfcb0b2de2
Victor Joaquin <vicjoaquin070@gmail.com>
2020-12-13 22:33:43 -0300

    snapshot tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer'
 import App from './App';
 import SinglePhoto from '../src/components/SinglePhoto'
 import Rovers from '../src/components/Rovers'
@@ -8,6 +9,7 @@ import ListPhotos from '../src/components/ListPhotos'
 import FilterBar from '../src/components/FilterBar'
 import DatePicker from './components/DatePicker';
 
+// Render Test
 test('renders App', () => {
   render(<App />)
 });
@@ -20,7 +22,7 @@ test('renders Rovers', () => {
   render(<Rovers />)
 })
 
-test('renders Rover Cards', () => {
+test('renders RoverCard', () => {
   render(<RoverCard />)
 })
 
@@ -28,7 +30,7 @@ test('renders Pagination', () => {
   render(<Pagination />)
 })
 
-test('renders List of Photos', () => {
+test('renders ListPhotos', () => {
   render(<ListPhotos />)
 })
 
@@ -38,4 +40,45 @@ test('renders FilterBar', () => {
 
 test('renders DatePicker', () => {
   render(<DatePicker />)
+})
+
+//Snapshot Test
+it("matches snapshot App", () => {
+  const tree = renderer.create(<App />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it("matches snapshot SinglePhoto", () => {
+  const tree = renderer.create(<SinglePhoto />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it("matches snapshot Rovers", () => {
+  const tree = renderer.create(<Rovers />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it("matches snapshot RoverCard", () => {
+  const tree = renderer.create(<RoverCard />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it("matches snapshot Pagination", () => {
+  const tree = renderer.create(<Pagination />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it("matches snapshot ListPhotos", () => {
+  const tree = renderer.create(<ListPhotos />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it("matches snapshot FilterBar", () => {
+  const tree = renderer.create(<FilterBar />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it("matches snapshot DatePicker", () => {
+  const tree = renderer.create(<DatePicker />).toJSON()
+  expect(tree).toMatchSnapshot()
 })
